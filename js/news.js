0b931a5308084ebcf7c6bcf5553d366259a56957
SyedAlamin <syedalamin2580@gmail.com>
2023-03-02 02:15:44 +0600

    some news card

--- js/news.js
+++ js/news.js
@@ -17,15 +17,66 @@ const showCategories = (data) => {
   });
 };
 
-const fetchCategoriesNews = (category_id , captegory_name) => {
+const fetchCategoriesNews = (category_id, captegory_name) => {
   const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => showAllNews(data.data , captegory_name));
+    .then((data) => showAllNews(data.data, captegory_name));
 };
 
-const showAllNews = (data, captegory_name)=>{
-  console.log(data, captegory_name)
+const showAllNews = (data, captegory_name) => {
+  // console.log(data, captegory_name)
   document.getElementById('news-count').innerText = data.length;
   document.getElementById('category-name').innerText = captegory_name;
+
+  const newsContainer = document.getElementById('all-news');
+  newsContainer.innerHTML = '';
+  data.forEach(singleNews => {
+    const { _id, image_url, title, details, author, total_view } = singleNews;
+    // newsContainer.innerHTML += ``;
+    const card = document.createElement('div');
+    card.classList.add('card', 'mb-3');
+    card.innerHTML = `
+    <div class="row g-0 ">
+    <div class="col-md-4">
+      <img src="${image_url}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8 d-flex flex-column">
+      <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text">
+        ${details.slice(0, 200) + "..."}
+        </p>
+      </div>
+      <div class="card-footer border-0 bg-body d-flex justify-content-between align-items-center">
+        <div class="d-flex gap-2">
+        <img src="${author.img}" class="img-fluid  rounded-circle" alt="..." height="40" width="40">
+          <div>
+            <p class="m-0 p-0">${author.name}</p>
+            <p class="m-0 p-0">${author.published_date}</p>
+          </div>
+          </div>
+          <div class="d-flex align-items-center gap-2">
+            <i class="fa-regular fa-eye"></i>
+            <p class="m-0 p-0">${total_view}</p>
+          </div>
+          <div>
+            <i class="fa-regular fa-star"></i>
+        </div>
+        <div>
+            <i class="fas fa-arrow-right" onclick="fetchNewsDetail('${_id}')"></i>
+        </div>
+      </div>
+    </div>
+    </div>
+    `;
+    newsContainer.appendChild(card);
+  });
+}
+
+const fetchNewsDetail = news_id => {
+  const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data.data[0]));
 }
